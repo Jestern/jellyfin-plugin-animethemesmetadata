322913a76f6d019487c75012fa01191f5b629976
Jestern <contact.jestern@proton.me>
2025-08-20 22:08:00 +0000

    Update to dotnet 9

--- scripts/build-jellyfin-source.sh
+++ scripts/build-jellyfin-source.sh
@@ -6,7 +6,7 @@ jellyfin_server_source_path="$1/jellyfin"
 jellyfin_web_source_path="$1/jellyfin-web"
 
 # Build jellyfin server
-dotnet build $jellyfin_server_source_path/Jellyfin.Server/Jellyfin.Server.csproj -o $jellyfin_server_source_path/bin/debug
+dotnet build $jellyfin_server_source_path/Jellyfin.Server/Jellyfin.Server.csproj
 
 # Build jellyfin web
 npm --prefix $jellyfin_web_source_path install
