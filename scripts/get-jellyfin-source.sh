7f686ab951ebb0d7cad1cd6ca84fe8001ebb23ce
Jestern <contact.jestern@proton.me>
2025-08-20 20:52:47 +0000

    fix jellyfin sources for Rider

--- scripts/get-jellyfin-source.sh
+++ scripts/get-jellyfin-source.sh
@@ -2,8 +2,8 @@
 
 source_path=$1
 
-jellyfin_server_git="git@github.com:jellyfin/jellyfin.git"
-jellyfin_web_git="git@github.com:jellyfin/jellyfin-web.git"
+jellyfin_server_git="https://github.com/jellyfin/jellyfin.git"
+jellyfin_web_git="https://github.com/jellyfin/jellyfin-web.git"
 
 mkdir -p $source_path
 git -C $source_path clone $jellyfin_server_git
